fb9bc7950ca7c8bc3ddf5a4885d1a485b4942225
Conroy95 <leandervdneut@ziggo.nl>
2025-05-02 18:52:42 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Service Worker registreren voor offline gebruik
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('js/service-worker.js')
+      .then(function (registration) {
+        console.log('ServiceWorker geregistreerd met scope: ', registration.scope);
+      }).catch(function (error) {
+        console.log('ServiceWorker registratie gefaald: ', error);
+      });
+  });
+}
+
+// Back to top
+function scrollToTop() {
+	window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+
+// Alleen datum tonen
+function toonDatum() {
+  const datumElement = document.getElementById("datum-tijd");
+  const nu = new Date();
+
+  const opties = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const datum = nu.toLocaleDateString('nl-NL', opties);
+
+  datumElement.textContent = `📅 Vandaag is: ${datum}`;
+}
+
+document.addEventListener("DOMContentLoaded", toonDatum);
---
+++ js/service-worker.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'roadtrip-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/script.js',
+  '/img/stad1.jpg',
+  '/img/stad2.jpg',
+  '/img/stad3.jpg',
+  '/img/stad4.jpg',
+  '/img/stad5.jpg',
+  '/img/stad6.jpg',
+  '/img/stad7.jpg',
+  '/img/stad8.jpg',
+  '/img/stad9.jpg',
+  '/img/stad10.jpg',
+  '/img/stad11.jpg',
+  '/img/stad12.jpg',
+  '/img/stad13.jpg',
+  // Voeg ALLE andere img's en dagen toe:
+  '/1.html',
+  '/2.html',
+  '/3.html',
+  '/4.html',
+  '/5.html',
+  '/6.html',
+  '/7.html',
+  '/8.html',
+  '/9.html',
+  '/10.html',
+  '/11.html',
+  '/12.html',
+  '/13.html',
+  '/14.html',
+  // etc. tot dag21.html
+  
+  // Favicons toevoegen:
+  '/fav/favicon-96x96.png',
+  '/fav/favicon.svg',
+  '/fav/favicon.ico',
+  '/fav/apple-touch-icon.png',
+  '/fav/site.webmanifest',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
